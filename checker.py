24a1a8a1c91c10b2cfc5106d8b79ffc7ada0bfc7
Ben Ranford <12677339+branford19@users.noreply.github.com>
2018-03-15 15:25:33 +1100

    Hello world

--- checker.py
+++ checker.py
@@ -7,4 +7,5 @@ from pprint import pprint
 companyname = input("Give the symbol of the company you would like to see the stock stats of (e.g. GOOGL for Alphabet Inc.")
 ts = TimeSeries(key='PIUH8SB6UWPJ7Q14', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol=companyname,interval='1min', outputsize='full')
+
 pprint(data.head(1))
