5aa781b6e0f4626d68be73a1cd4096211a195c61
Ben Ranford <12677339+branford19@users.noreply.github.com>
2018-03-15 15:48:16 +1100

    Fixed grammar

--- checker.py
+++ checker.py
@@ -4,7 +4,7 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 
 # Input the name of the company
-companyname = input("Give the symbol of the company you would like to see the stock stats of (e.g. GOOGL for Alphabet Inc.")
+companyname = input("Give the symbol of the company you would like to see the stock stats of (e.g. GOOGL for Alphabet Inc.): ")
 ts = TimeSeries(key='PIUH8SB6UWPJ7Q14', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol=companyname,interval='1min', outputsize='full')
 
