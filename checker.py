f95d73475c1a6bb6baf70ba7700a065f33526abb
Ben Ranford <12677339+branford19@users.noreply.github.com>
2018-03-16 12:46:17 +1100

    Added a call to the Yahoo Finance API
    Used to find company name from symbol and then display that to the user.

--- checker.py
+++ checker.py
@@ -2,9 +2,27 @@ import pandas
 import json
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
+import requests
+
+companyname2 = input("What is the name of the company you'd like stock prices for? ")
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+
+
+company = get_symbol(companyname2)
+
+print("Cool, " + company + " it is!")
+print("")
 
 # Input the name of the company
-companyname = input("Give the symbol of the company you would like to see the stock stats of (e.g. GOOGL for Alphabet Inc.): ")
+companyname = companyname2
+
 ts = TimeSeries(key='PIUH8SB6UWPJ7Q14', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol=companyname,interval='1min', outputsize='full')
 
